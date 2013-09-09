Numergy/yoda

commit ff4d289c3fff3db623c8f1d1f930c65a4f234752

Write class name in CamelCase

yoda/subcommands.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-class subcommand:
+class Subcommand:
     config = None
     subparser = None
 
@@ -10,7 +10,7 @@ class subcommand:
         self.subparser = subparser
 
 
-class subcommands:
+class Subcommands:
     config = None
     parser = None
     subparser = None

tests/subcommands_test.py
@@ -2,8 +2,8 @@ import unittest
 
 from mock import Mock
 
-from yoda.subcommands import subcommand
-from yoda.subcommands import subcommands
+from yoda.subcommands import Subcommand
+from yoda.subcommands import Subcommands
 
 
 class TestSubcommand(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestSubcommand(unittest.TestCase):
 
     def test_setup(self):
         """ Test subcommand's attributes setup """
-        subcmd = subcommand()
+        subcmd = Subcommand()
         subcmd.setup("foo", "bar")
         self.assertEqual("foo", subcmd.config)
         self.assertEqual("bar", subcmd.subparser)
@@ -30,7 +30,7 @@ class TestSubcommands(unittest.TestCase):
         self.mocks["subcmd"].execute.return_value = None
 
         self.mocks["config"] = Mock()
-        self.subcmds = subcommands(self.mocks["config"])
+        self.subcmds = Subcommands(self.mocks["config"])
 
     def tearDown(self):
         """ Tear down test suite """

tests/output_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from mock import Mock
-from yoda.output import output
+from yoda.output import Output
 
 
 class TestOutput(unittest.TestCase):
@@ -16,7 +16,7 @@ class TestOutput(unittest.TestCase):
         self.stdout = self.stderr = Mock()
         self.stdout.write.return_value = None
         self.stderr.write.return_value = None
-        self.out = output(self.stdout, self.stderr)
+        self.out = Output(self.stdout, self.stderr)
 
     def test_info(self):
         """ Test printing an information message """

tests/subcommand/workspace_test.py
@@ -3,10 +3,10 @@ import argparse
 
 from mock import Mock
 from ..utils import mock_config
-from yoda.subcommand.workspace import workspace
+from yoda.subcommand.workspace import Workspace
 
 
-class TestSubcommandsWorkspace(unittest.TestCase):
+class TestSubcommandWorkspace(unittest.TestCase):
     """ Workspace subcommand test suite """
     parser = None
     workspace = None
@@ -17,7 +17,7 @@ class TestSubcommandsWorkspace(unittest.TestCase):
         subparser = self.parser.add_subparsers(dest="subcommand_test")
 
         config_data = "{workspaces: {}}"
-        self.workspace = workspace()
+        self.workspace = Workspace()
         self.workspace.setup(mock_config(config_data), subparser)
 
     def tearDown(self):

tests/workspace_test.py
@@ -2,7 +2,7 @@ import unittest
 import os.path
 
 from .utils import mock_config
-from yoda.workspace import workspace
+from yoda.workspace import Workspace
 
 
 class TestWorkspace(unittest.TestCase):
@@ -11,35 +11,35 @@ class TestWorkspace(unittest.TestCase):
     def test_add(self):
         """ Test add workspace """
         config = mock_config({"workspaces": {"foo": "/foo"}})
-        ws = workspace(config)
+        ws = Workspace(config)
         ws.add("bar", os.path.realpath(__file__))
         config.write.assert_called_once()
 
     def test_add_existing_workspace(self):
         """ Test add workspace with existing name """
         config = mock_config({"workspaces": {"foo": "/foo"}})
-        ws = workspace(config)
+        ws = Workspace(config)
         self.assertRaises(
             ValueError, ws.add, "foo", os.path.realpath(__file__))
 
     def test_add_with_nonexistent_path(self):
         """ Test add workspace with path doesn't exists """
         config = mock_config({"workspaces": {}})
-        ws = workspace(config)
+        ws = Workspace(config)
         self.assertRaises(
             ValueError, ws.add, "foo", "/dir/doesnt/exists")
 
     def test_remove(self):
         """ Test remove workspace """
         config = mock_config({"workspaces": {"foo": "/foo"}})
-        ws = workspace(config)
+        ws = Workspace(config)
         ws.remove("foo")
         config.write.assert_called_once()
 
     def test_remove_nonexistent(self):
         """ Test remove workspace that doesn't exists """
         config = mock_config({"workspaces": {"foo": "/foo"}})
-        ws = workspace(config)
+        ws = Workspace(config)
         self.assertRaises(
             ValueError, ws.remove, "bar")
 
@@ -47,7 +47,7 @@ class TestWorkspace(unittest.TestCase):
         """ Test list workspace """
         config_mock_data = {"workspaces": {"foo": "/foo", "bar": "/bar"}}
 
-        ws = workspace(mock_config(config_mock_data))
+        ws = Workspace(mock_config(config_mock_data))
         list = ws.list()
         self.assertIn("foo", list)
         self.assertIn("bar", list)
@@ -57,11 +57,11 @@ class TestWorkspace(unittest.TestCase):
     def test_exists(self):
         """ Test exists workspace """
         config_mock_data = {"workspaces": {"foo": "/foo"}}
-        ws = workspace(mock_config(config_mock_data))
+        ws = Workspace(mock_config(config_mock_data))
         self.assertTrue(ws.exists("foo"))
 
     def test_not_exists(self):
         """ Test workspace doesn't exists"""
         config_mock_data = {"workspaces": {"foo": "/foo"}}
-        ws = workspace(mock_config(config_mock_data))
+        ws = Workspace(mock_config(config_mock_data))
         self.assertFalse(ws.exists("bar"))

yoda.py
@@ -2,16 +2,16 @@
 
 import os
 
-from yoda.config import config
-from yoda.output import output
-from yoda.subcommands import subcommands
-from yoda.subcommand.workspace import workspace
+from yoda.config import Config
+from yoda.output import Output
+from yoda.subcommands import Subcommands
+from yoda.subcommand.workspace import Workspace
 
-yoda_config = config("%s/.yodarc" % os.environ.get("HOME"))
-out = output()
+yoda_config = Config("%s/.yodarc" % os.environ.get("HOME"))
+out = Output()
 
-subcmd = subcommands(yoda_config)
-subcmd.add_command(workspace())
+subcmd = Subcommands(yoda_config)
+subcmd.add_command(Workspace())
 parser = subcmd.parse()
 args = subcmd.parser.parse_args()
 

yoda/config.py
@@ -2,7 +2,7 @@ import yaml
 import os.path
 
 
-class config:
+class Config:
 
     config_file = None
 
@@ -21,8 +21,10 @@ class config:
         return config
 
     def write(self, data):
-        """ Write config in configuration file.
-        Given config must me a dict"""
+        """
+        Write config in configuration file.
+        Data must me a dict.
+        """
         file = open(self.config_file, "w+")
         file.write(yaml.dump(data))
         file.close()

yoda/output.py
@@ -1,7 +1,7 @@
 import sys
 
 
-class output:
+class Output:
     GREEN = "\033[92m%s\033[0m"
     YELLOW = "\033[93m%s\033[0m"
     RED = "\033[91m%s\033[0m"

commit 0d648e769e3a7d980ddcc4384226916c0f1aea2e

Fixed bug introduced in changeset ff4d289
This bug is caused by CamelCase class naming.

yoda/subcommands.py
@@ -24,7 +24,7 @@ class Subcommands:
 
     def add_command(self, command):
         command.setup(self.config, self.subparser)
-        self.commands[command.__class__.__name__] = command
+        self.commands[command.__class__.__name__.lower()] = command
 
     def parse(self):
         for key, command in self.commands.items():

tests/subcommands_test.py
@@ -41,8 +41,8 @@ class TestSubcommands(unittest.TestCase):
         """ Test add subcommand """
         self.subcmds.add_command(self.mocks["subcmd"])
 
-        self.assertTrue("Mock" in self.subcmds.commands)
-        self.assertEqual(self.mocks["subcmd"], self.subcmds.commands["Mock"])
+        self.assertTrue("mock" in self.subcmds.commands)
+        self.assertEqual(self.mocks["subcmd"], self.subcmds.commands["mock"])
 
     def test_parse(self):
         """ Test parse subcommand """